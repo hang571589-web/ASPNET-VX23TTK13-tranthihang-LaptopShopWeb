5add59b56c849c0a4ab81bb49b447dbd9327681e
hang571589-web <hang571589@gmail.com>
2025-11-09 18:04:28 +0700

    add docker-compose

--- src/LaptopShopWeb/LaptopShopWeb/obj/Debug/net9.0/LaptopShopWeb.AssemblyInfo.cs
+++ src/LaptopShopWeb/LaptopShopWeb/obj/Debug/net9.0/LaptopShopWeb.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LaptopShopWeb")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9e341975d72bbb70978b801bd8ce4b9acb01b0a2")]
 [assembly: System.Reflection.AssemblyProductAttribute("LaptopShopWeb")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LaptopShopWeb")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
